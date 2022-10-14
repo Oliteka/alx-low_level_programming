fe70a44b898e5ff87b72e23411205b4658e7ff33
oliteka <olitekae.v@gmail.com>
2022-10-13 17:26:34 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
+/* main - This is the entry of the program */
 #include <stdio.h>
-
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
-	return(0);
-}	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
